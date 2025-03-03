54e302d2fa567b0367a66de22c637d44184310f7
ekajaya740 <ekajaya740@gmail.com>
2025-03-03 15:10:45 +0800

    fix: script

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,8 +1,6 @@
-# docker build -t ekajaya740/karsajobs:latest .
-# docker tag ekajaya740/karsajobs:latest ghcr.io/ekajaya740/karsajobs:latest
+docker build -t ekajaya740/karsajobs:latest .
+docker tag ekajaya740/karsajobs:latest ghcr.io/ekajaya740/karsajobs:latest
 
-echo "${GITHUB_TOKEN} My token"
+echo "${GITHUB_TOKEN}" | docker login --username ekajaya740 --password-stdin
 
-# echo "${GITHUB_TOKEN}" | docker login --username ekajaya740 --password-stdin
-#
-# docker push ghcr.io/ekajaya740/karsajobs:latest
+docker push ghcr.io/ekajaya740/karsajobs:latest
