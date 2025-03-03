8317529bfef7513d020ba79fb662d11318e273e2
ekajaya740 <ekajaya740@gmail.com>
2025-03-03 14:21:38 +0800

    fix: inspect token

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,7 +1,7 @@
 docker build -t ekajaya740/karsajobs:latest .
 docker tag ekajaya740/karsajobs:latest ghcr.io/ekajaya740/karsajobs:latest
 
-echo "${GITHUB_TOKEN}"
+echo "TOKEN hehe ${GITHUB_TOKEN}"
 echo "${GITHUB_TOKEN}" | docker login --username ekajaya740 --password-stdin
 
 docker push ghcr.io/ekajaya740/karsajobs:latest
