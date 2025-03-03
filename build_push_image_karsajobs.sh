df23b09b1c444ff7783a4d1835c3d9f6c984c9b9
ekajaya740 <ekajaya740@gmail.com>
2025-03-03 14:30:54 +0800

    fix: add args

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,7 +1,8 @@
 docker build -t ekajaya740/karsajobs:latest .
 docker tag ekajaya740/karsajobs:latest ghcr.io/ekajaya740/karsajobs:latest
 
-echo "TOKEN hehe ${GITHUB_TOKEN} "
+GITHUB_TOKEN=$1
+
 echo "${GITHUB_TOKEN}" | docker login --username ekajaya740 --password-stdin
 
 docker push ghcr.io/ekajaya740/karsajobs:latest
