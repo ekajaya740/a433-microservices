4dfd75b963c9384ef0f2e1ef20226e2eae040d60
ekajaya740 <ekajaya740@gmail.com>
2025-03-04 09:02:57 +0800

    docs: add docs

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,6 +1,6 @@
-docker build -t ekajaya740/karsajobs-ui:latest .
-docker tag ekajaya740/karsajobs-ui:latest ghcr.io/ekajaya740/karsajobs-ui:latest
+docker build -t ekajaya740/karsajobs-ui:latest .                                 # Build image menggunakan Dockerfile yang ada pada directory ini dengan tag ekajaya740/karsajobs-ui:latest
+docker tag ekajaya740/karsajobs-ui:latest ghcr.io/ekajaya740/karsajobs-ui:latest # Ubah tag dari ekajaya740/karsajobs-ui:latest ke ghcr.io/ekajaya740/karsajobs-ui:latest agar bisa di terima oleh GHCR
 
-echo "${GITHUB_TOKEN}" | docker login ghcr.io --username ekajaya740 --password-stdin
+echo "${GITHUB_TOKEN}" | docker login ghcr.io --username ekajaya740 --password-stdin # Login ke ghcr.io dengan username dan password dari env pada GitHub Actions
 
-docker push ghcr.io/ekajaya740/karsajobs-ui:latest
+docker push ghcr.io/ekajaya740/karsajobs-ui:latest # Push image ke ghcr
